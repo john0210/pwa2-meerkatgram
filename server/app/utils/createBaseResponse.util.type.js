0246ae39351f6fd91ece7dae6607fda98155291e
kimjunghyun <john0210@hanmail.net>
2025-11-19 17:51:13 +0900

    251119 meerkatgram-3

---
+++ server/app/utils/createBaseResponse.util.type.js
@@ -0,0 +1,15 @@
+/**
+ * @file app/utils/createBaseResponse.util.type.js
+ * @description 공통 응답 형식을 생성하는 유틸리티 타입 정의
+ * 251119 v1.0.0 park init
+ */
+
+/**
+ * 응답 데이터 생성 유틸리티 타입
+ * @typedef {object} CreateBaseResponse
+ * @property {string} code - 응답 코드
+ * @property {string} msg - 응답 상세 메시지
+ * @property {null|[]|{}} data - 데이터
+ */
+
+export {};
