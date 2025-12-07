2afb46a28fdaeab8f087a7e77de8cbd5d6201792
Kim junghyun <john0210@hanmail.net>
2025-12-08 07:57:36 +0900

    251208 meerkatgram-18 homework

--- client/src/components/posts/PostIndex.jsx
+++ client/src/components/posts/PostIndex.jsx
@@ -3,6 +3,7 @@ import './PostIndex.css';
 import { useEffect } from 'react';
 import { postIndexThunk } from '../../store/thunks/postIndexThunk.js';
 import { useNavigate } from 'react-router-dom';
+import { clearPostIndex } from '../../store/slices/postIndexSlice.js';
 
 export default function PostIndex() {
   const navigate = useNavigate();
@@ -10,8 +11,10 @@ export default function PostIndex() {
   const { list, page, isLasted } = useSelector(state => state.postIndex);
 
   useEffect(() => {
-    if(!list) {
-      dispatch(postIndexThunk(page + 1));
+    dispatch(postIndexThunk(page + 1));
+
+    return () => {
+      dispatch(clearPostIndex());
     }
   }, []);
 
