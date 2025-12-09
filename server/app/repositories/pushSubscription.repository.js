6a0be664e91b7c7964b2f2f3e1b08b8154c565f8
kimjunghyun <john0210@hanmail.net>
2025-12-09 17:46:55 +0900

    251209 meekatgram-20

--- server/app/repositories/pushSubscription.repository.js
+++ server/app/repositories/pushSubscription.repository.js
@@ -11,6 +11,28 @@ async function upsert(t= null, data) {
  return await PushSubscription.upsert(data, {transaction: t}); 
 }
 
+async function findByUserId(t = null, userId) {
+  return await PushSubscription.findAll(
+    {
+      where: {
+        userId: userId
+      }
+    },
+    {
+      transaction: t
+    }
+  ); 
+}
+async function hardDestroy(t = null, id) {
+  return await PushSubscription.destory({
+    where: {id: id},
+    force: true,
+    transaction: t,
+  });
+}
+
 export default {
   upsert,
+  findByUserId,
+  hardDestroy,
 }
